aaee7e0d2724fb055630f41edfa9a39a728c7dc0
mpottebaum <mpottebaum@gmail.com>
2020-11-06 10:27:37 -0600

    added route namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # TEAM
-  post '/teams', to: 'teams#create'
-
-  # LOGIN
-  post '/auth', to: 'auth#create'
-  # VALIDATE TOKEN
-  get '/auth', to: 'auth#show'
-
-  # ROSTER CRUD
-  post '/teams/:id/rosters', to: 'rosters#create'
-  get '/teams/:id/rosters', to: 'rosters#show'
-  put '/teams/:id/rosters', to: 'rosters#update'
-  delete '/teams/:id/rosters', to: 'rosters#destroy'
-
-  # BOTS
-  get '/teams/:id/bots', to: 'bots#index'
+  namespace :api do
+    namespace :v1 do
+      # TEAM
+      post '/teams', to: 'teams#create'
+    
+      # LOGIN
+      post '/auth', to: 'auth#create'
+      # VALIDATE TOKEN
+      get '/auth', to: 'auth#show'
+    
+      # ROSTER CRUD
+      post '/teams/:id/rosters', to: 'rosters#create'
+      get '/teams/:id/rosters', to: 'rosters#show'
+      put '/teams/:id/rosters', to: 'rosters#update'
+      delete '/teams/:id/rosters', to: 'rosters#destroy'
+    
+      # BOTS
+      get '/teams/:id/bots', to: 'bots#index'
+    end
+  end
 end
