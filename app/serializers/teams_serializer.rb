3c4cca6b7a3ae82908f79a5809cb4780f5ea4318
mpottebaum <mpottebaum@gmail.com>
2020-11-07 18:26:42 -0600

    added saved_roster boolean to team resp object

--- app/serializers/teams_serializer.rb
+++ app/serializers/teams_serializer.rb
@@ -1,3 +1,8 @@
 class TeamsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
+  attributes :id, :name, :email, :saved_roster
+
+  def saved_roster
+    !!object.roster
+  end
+
 end
